7230d6ed3db5b12da56156de75a01ff30264bc1b
Nirupama Sharma <nirupamasharma@Nirupamas-MBP.fios-router.home>
2018-08-26 03:12:56 -0400

    adding files

--- myprofile/src/components/Home.js
+++ myprofile/src/components/Home.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
 import {FormGroup, Input, Label, Col} from 'reactstrap';
 import HeaderUser from "./HeaderUser";
-import image1 from './image.png'
-import image2 from './image6.png'
-import image3 from './image14.png'
-import image4 from './image9.png'
+import image1 from './image14.png'
+import image2 from './image9.png'
+import image3 from './image6.png'
+import image4 from './image.png'
 import image5 from './plag1.png'
 import image6 from './plag2.png'
 import image7 from './plag3.png'
@@ -26,21 +26,22 @@ class Home extends Component {
                 <form className="container">
                     <div className="container my-carousel" style={{border:'thin'}}>
                     <Carousel axis="horizontal" showArrows={true}>
+
                         <div>
                             <img src={image1} />
-                            <p className="legend">Design & Share React Redux Web Application</p>
-                        </div>
-                        <div>
-                            <img src={image2} />
-                            <p className="legend">Design & Share React Redux Web Application</p>
-                        </div>
-                        <div>
-                            <img src={image3} />
                             <p className="legend">Robotics Motion Planning using PRM and RRT Algorithms for Puma560</p>
                         </div>
                          <div>
-                            <img src={image4} />
+                            <img src={image2} />
                             <p className="legend">Robotics Motion Planning using PRM Algorithm for Puma560</p>
+                        </div>
+                        <div>
+                            <img src={image3} />
+                            <p className="legend">Design & Share React Redux Web Application</p>
+                        </div>
+                        <div>
+                            <img src={image4} />
+                            <p className="legend">Design & Share React Redux Web Application</p>
                         </div>
                         <div>
                             <img src={image5} />
