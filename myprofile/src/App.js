de18095955f1c6fcacb6ce0f963716752c8f1708
Nirupama Sharma <nirupamasharma@Nirupamas-MBP.fios-router.home>
2018-08-26 05:25:03 -0400

    adding files

--- myprofile/src/App.js
+++ myprofile/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Home from "./components/Home1";
+import Home from "./components/Home";
 
 class App extends Component {
   render() {
